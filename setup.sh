04ba0db300d5b09b5f4159568c3e557ba80c2c3a
Maxim Avanov <maxim.avanov@gmail.com>
2015-01-18 21:25:50 +0300

    Add haskell-platform

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-read -p "Setup as user: " -e USERNAME
+read -p "Setup as user [$USER]: " -e USERNAME
+USERNAME=${USERNAME:-$USER}
+
 
 ansible-playbook ./default.yml -i ./hosts --user=$USERNAME -vv -K
